56c94e11a0374befd44c9fce273727da67313954
Eva Pace <eba.pachi@gmail.com>
2025-11-05 05:01:47 -0300

    eight: part one complete

--- eight/bin/main.ml
+++ eight/bin/main.ml
@@ -1,9 +1,13 @@
 let () =
-  let ic = open_in "example" in
-  try
-    while true do
+  let ic = open_in "input" in
+  let rec aux acc =
+    try
       let line = input_line ic in
       let code, mem = Eight.char_counts line in
-      Printf.printf "The number is: %d, %d\n" code mem
-    done
-  with End_of_file -> close_in ic
+      (* Printf.printf "The number is: %d, %d\n" code mem *)
+      aux (acc + code - mem)
+    with End_of_file ->
+      close_in ic;
+      acc
+  in
+  Printf.printf "%d\n" (aux 0)
